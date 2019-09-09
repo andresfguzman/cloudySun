52bab350af2f895477c681d99fba94982440ac44
Andrés Guzmán <andresguzman@MacBook-Pro-de-Andres.local>
2019-09-09 17:31:29 -0500

    First commit

---
+++ CloudySun/CloudySun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudySun
+//
+//  Created by Andrés Guzmán on 9/9/19.
+//  Copyright © 2019 Andres Felipe Guzman Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
